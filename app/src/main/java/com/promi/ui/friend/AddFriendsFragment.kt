a7115cbcc6dfe89138c3836804e8a5d78f1c530b
Namsang710 <nyh710@gmail.com>
2023-10-07 23:29:25 +0900

    DESIGN/Create fragment_add_friends
    친구 추가 화면 개발

---
+++ app/src/main/java/com/promi/ui/friend/AddFriendsFragment.kt
@@ -0,0 +1,22 @@
+package com.promi.ui.friend
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.promi.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AddFriendsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AddFriendsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
