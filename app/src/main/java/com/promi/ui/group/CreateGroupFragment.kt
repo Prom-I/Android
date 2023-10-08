00b6447f53f8bc9e13defd689b36e61e0e5c86e7
Namsang710 <nyh710@gmail.com>
2023-10-08 11:23:55 +0900

    DESIGN/Create fragment_create_group
    그룹 생성하기 화면 개발(37번 화면, 38번 화면)

--- app/src/main/java/com/promi/ui/group/CreateGroupFragment.kt
+++ app/src/main/java/com/promi/ui/group/CreateGroupFragment.kt
@@ -1,25 +1,17 @@
-package com.promi.ui.friend
+package com.promi.ui.group
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.promi.R
 import com.promi.databinding.FragmentAddFriendsBinding
-import com.promi.databinding.FragmentMyInformationBinding
+import com.promi.databinding.FragmentCreateGroupBinding
 import com.promi.ui.myInformation.MyInformationViewModel
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddFriendsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class AddFriendsFragment : Fragment() {
-
-    private var _binding: FragmentAddFriendsBinding? = null
+class CreateGroupFragment : Fragment(){
+    private var _binding: FragmentCreateGroupBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -33,7 +25,7 @@ class AddFriendsFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(MyInformationViewModel::class.java)
 
-        _binding = FragmentAddFriendsBinding.inflate(inflater, container, false)
+        _binding = FragmentCreateGroupBinding.inflate(inflater, container, false)
 
         return binding.root
     }
@@ -42,5 +34,4 @@ class AddFriendsFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
 }
