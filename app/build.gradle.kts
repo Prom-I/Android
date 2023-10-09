29c8cfd702fac84f6fd5ae418aedc10611f34d42
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-10 01:34:31 +0900

    DOC/Add ViewPager2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,10 +40,16 @@ android {
 }
 
 dependencies {
+    // viewPager2
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     // LiveData
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    // 원형 이미지
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -54,6 +60,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
