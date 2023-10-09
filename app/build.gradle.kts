78af33858e83e28a6985a5499fede2c1e5ea5103
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-10-09 22:01:16 +0900

    BLD/Add Comment-LifeCycle, ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,18 +34,21 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
+        dataBinding = true
         viewBinding = true
     }
 }
 
 dependencies {
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     testImplementation("junit:junit:4.13.2")
