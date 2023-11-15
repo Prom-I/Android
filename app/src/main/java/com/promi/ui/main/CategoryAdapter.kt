e7820e54dc64c437285900a5df159e12dfc3bf6f
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-11-16 02:19:22 +0900

    FEAT/add Add Todo Dialog

--- app/src/main/java/com/promi/ui/main/CategoryAdapter.kt
+++ app/src/main/java/com/promi/ui/main/CategoryAdapter.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.promi.databinding.ItemCategoryBinding
 import com.promi.ui.calendar.CalendarAdapter
@@ -21,6 +24,7 @@ class CategoryAdapter() :
         :RecyclerView.ViewHolder(itemViewBinding.root){
         val dividerHorizontal: View = itemViewBinding.dividerHorizontal
         val dividerVertical: View = itemViewBinding.dividerVertical
+        val layoutCategory: ConstraintLayout = itemViewBinding.layoutCategory
     }
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -39,8 +43,23 @@ class CategoryAdapter() :
     override fun getItemCount(): Int { return itemList.size}
 
     override fun onBindViewHolder(holder: CategoryAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
         if (position % 2 == 0)
             holder.dividerHorizontal.visibility = View.GONE
+
+        // (1) 리스트 내 항목 클릭 시 onClick() 호출
+        holder.layoutCategory.setOnClickListener {
+            itemClickListener?.onClick(it, position)
+        }
     }
+    // (2) 리스너 인터페이스
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
+    // (3) 외부에서 클릭 시 이벤트 설정
+    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
+    }
+
+    // (4) setItemClickListener로 설정한 함수 실행
+    private var itemClickListener : OnItemClickListener? = null
 }
--- app/src/main/java/com/promi/ui/main/CategoryDialog.kt
+++ app/src/main/java/com/promi/ui/main/CategoryDialog.kt
@@ -1,9 +1,11 @@
 package com.promi.ui.main
 
+import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import com.promi.R
 import com.promi.base.BaseBottomSheetDialog
 import com.promi.databinding.DialogCategoryBinding
+import com.promi.ui.calendar.CalendarAdapter
 
 class CategoryDialog:BaseBottomSheetDialog<DialogCategoryBinding> (R.layout.dialog_category) {
     private lateinit var categoryAdapter: CategoryAdapter
@@ -11,10 +13,21 @@ class CategoryDialog:BaseBottomSheetDialog<DialogCategoryBinding> (R.layout.dial
         super.initStartView()
 
         categoryAdapter = CategoryAdapter()
+        categoryAdapter.itemList.add("1")
 
         binding.listCategory.apply {
             adapter = categoryAdapter
             layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
         }
+    }
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+        categoryAdapter.setItemClickListener(object: CategoryAdapter.OnItemClickListener {
+            override fun onClick(v: View, position: Int) {
+                AddTodoDialog().show(parentFragmentManager, "AddTodoDialog")
+            }
+        })
     }
 }
