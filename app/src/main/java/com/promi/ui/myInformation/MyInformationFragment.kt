9570f2309395a1eb5ddc34fc93217ac951731cc8
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-15 16:34:29 +0900

    ADD/ Setting button icon & Notification button icon svg vector image

--- app/src/main/java/com/promi/ui/myInformation/MyInformationFragment.kt
+++ app/src/main/java/com/promi/ui/myInformation/MyInformationFragment.kt
@@ -28,10 +28,6 @@ class MyInformationFragment : Fragment() {
         _binding = FragmentMyInformationBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
