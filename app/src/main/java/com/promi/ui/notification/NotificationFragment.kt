997312c49e5ec8374a2e1e689cf9286fb83e8e36
Namsang710 <nyh710@gmail.com>
2023-11-20 11:45:24 +0900

    DESGIN/Notification

---
+++ app/src/main/java/com/promi/ui/notification/NotificationFragment.kt
@@ -0,0 +1,17 @@
+package com.promi.ui.notification
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.promi.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [NotificationFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class NotificationFragment : Fragment() {
+
+}
