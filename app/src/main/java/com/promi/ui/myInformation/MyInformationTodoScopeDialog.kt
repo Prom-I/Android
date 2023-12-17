17498daee9f71a4c309e1862794ff342d59f46c8
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-12-17 21:08:28 +0900

    FEAT / create TodoScopeDialog

---
+++ app/src/main/java/com/promi/ui/myInformation/MyInformationTodoScopeDialog.kt
@@ -0,0 +1,9 @@
+package com.promi.ui.myInformation
+
+import com.promi.R
+import com.promi.base.BaseBottomSheetDialog
+import com.promi.databinding.DialogInformationTodoScopeBinding
+
+class MyInformationTodoScopeDialog:BaseBottomSheetDialog<DialogInformationTodoScopeBinding> (R.layout.dialog_information_todo_scope) {
+
+}
