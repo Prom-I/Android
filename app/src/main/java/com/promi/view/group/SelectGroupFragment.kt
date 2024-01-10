037da8d2a4421772979490ef4cb855d985c10dc7
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-10 17:52:25 +0900

    ADD : Select Group Fragment & Item Select Group layout

---
+++ app/src/main/java/com/promi/view/group/SelectGroupFragment.kt
@@ -0,0 +1,24 @@
+package com.promi.view.group
+
+import com.promi.R
+import com.promi.base.BaseFragment
+import com.promi.databinding.FragmentSelectGroupBinding
+
+class SelectGroupFragment : BaseFragment<FragmentSelectGroupBinding>(R.layout.fragment_select_group) {
+
+
+    // * 레이아웃을 띄운 직후 호출. * 뷰나 액티비티의 속성 등을 초기화. * ex) 리사이클러뷰, 툴바, 드로어뷰..
+    override fun initStartView() {
+
+    }
+    // * 데이터 바인딩 설정.
+    override fun initDataBinding() {
+
+    }
+    // * 바인딩 이후에 할 일을 여기에 구현. * 그 외에 설정할 것이 있으면 이곳에서 설정. * 클릭 리스너도 이곳에서 설정.
+    override fun initAfterBinding() {
+
+    }
+
+}
+
