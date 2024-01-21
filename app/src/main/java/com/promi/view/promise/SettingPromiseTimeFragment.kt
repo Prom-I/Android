5493ccb7e9d88853470f553d69ea1dc3663a69a2
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-01-21 18:26:40 +0900

    CLN / BaseFragment 적용

---
+++ app/src/main/java/com/promi/view/promise/SettingPromiseTimeFragment.kt
@@ -0,0 +1,9 @@
+package com.promi.view.promise
+
+import com.promi.R
+import com.promi.base.BaseFragment
+import com.promi.databinding.FragmentSettingPromiseTimeBinding
+
+class SettingPromiseTimeFragment : BaseFragment<FragmentSettingPromiseTimeBinding>(R.layout.fragment_setting_promise_time) {
+
+}
