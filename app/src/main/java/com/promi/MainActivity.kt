da4e038f6db3bb66ac25b5df11f9f903edc2f3c2
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-01-23 03:35:33 +0900

    FEAT / fragment->androidx.containerView 변경으로 인한 navigation 방식 변경

--- app/src/main/java/com/promi/MainActivity.kt
+++ app/src/main/java/com/promi/MainActivity.kt
@@ -3,13 +3,15 @@ package com.promi
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.promi.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +22,9 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
         // 상단 탭의 값을 변경하는 코드 => 나중에 쓸 일 있으면 주석 풀 것
         // Passing each menu ID as a set of Ids because each
@@ -36,14 +40,13 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
-    fun setToolbar(isVisible: Boolean, name: String?) {
-        if (isVisible)
+    fun setToolbar(isVisible: Boolean = false, name: String = "") {
+        if (isVisible) {
+            binding.btnToolbarBack.setOnClickListener { onBackPressed() }
+            binding.tvToolbarName.text = name
             binding.toolbar.visibility = View.VISIBLE
+        }
         else
             binding.toolbar.visibility = View.GONE
-
-        binding.btnBack.setOnClickListener { onBackPressed() }
-
-        binding.tvToolbarName.text = name
     }
 }
