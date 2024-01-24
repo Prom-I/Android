59fac2f3eabbeb00c55bc8ee8a861f2b002dcabb
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-24 14:35:04 +0900

    FEAT : Attach TimeTextViewRecyclerView Time

--- app/src/main/java/com/promi/view/promise/ViewPromiseTimeFragment.kt
+++ app/src/main/java/com/promi/view/promise/ViewPromiseTimeFragment.kt
@@ -1,45 +1,36 @@
 package com.promi.view.promise
 
-import android.util.Log
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.promi.MainActivity
 import com.promi.R
 import com.promi.base.BaseFragment
 import com.promi.databinding.FragmentViewPromiseTimeBinding
+import com.promi.view.promise.adapter.TimeTextViewRecyclerViewAdapter
 
 class ViewPromiseTimeFragment : BaseFragment<FragmentViewPromiseTimeBinding>(R.layout.fragment_view_promise_time) {
 
     // 시간 텍스트뷰 리사이클러뷰(왼쪽에 시간 보여주는 용도)
     private lateinit var timeTextViewRecyclerView : RecyclerView
+    private lateinit var timeTextViewRecyclerViewAdapter : TimeTextViewRecyclerViewAdapter
     // 시간 리사이클러뷰(핵심 기능, 민트색으로 시간정보를 보여주는 부분)
     private lateinit var promiseTimeRecyclerView : RecyclerView
     // 추천 약속 시간
     private lateinit var recommendTimeRecyclerView : RecyclerView
 
+    private var timeSize : Int = 10 // 시간 범위(2차원 배열의 높이, 몇시부터 몇시까지인지)
+    private var daySize : Int = 0 // 며칠을 보여줄 것인지(promiseTimeRecyclerView에서 사용, 2차원 배열의 가로)
+
     override fun initStartView() {
         (activity as MainActivity).setToolbar(true, "1차 회의")
         addToToolbar()
 
-//        // 뷰 모델 가져오기
-//        groupViewModel = ViewModelProvider(requireActivity())[GroupViewModel::class.java]
-//        Log.d("SelectGroupFragment Error #1","${groupViewModel.groups.value}")
-//
-//        // recyclerview init
-//        initRecyclerView()
-//
-//        // observe data
-//        groupViewModel.groups.observe(viewLifecycleOwner) { group ->
-//
-//            selectGroupRecyclerViewAdapter.updateData(group)
-//
-//        }
+        // 뷰 모델 가져오기
     }
-
-
 
     // * 데이터 바인딩 설정.
     override fun initDataBinding() {
@@ -60,7 +51,10 @@ class ViewPromiseTimeFragment : BaseFragment<FragmentViewPromiseTimeBinding>(R.l
     }
 
     private fun initTimeTextRecyclerView(){
-
+        // timeSize만큼 리사이클러뷰 그리기
+        timeTextViewRecyclerViewAdapter = TimeTextViewRecyclerViewAdapter(timeSize,"09:00")
+        timeTextViewRecyclerView.adapter = timeTextViewRecyclerViewAdapter // 어뎁터 부착
+        timeTextViewRecyclerView.layoutManager = LinearLayoutManager(this.context)
     }
 
     private fun initPromiseTimeRecyclerView(){
