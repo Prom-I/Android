91ba39291b83f3cddcc92b7530736372293142a6
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-02-09 19:15:14 +0900

    FEAT : Add ImageBtn for popup menu - tool bar

--- app/src/main/java/com/promi/view/promise/RecommendTimeDetailFragment.kt
+++ app/src/main/java/com/promi/view/promise/RecommendTimeDetailFragment.kt
@@ -2,6 +2,7 @@ package com.promi.view.promise
 
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
@@ -16,7 +17,7 @@ import com.promi.databinding.FragmentRecommendTimeDetailBinding
 class RecommendTimeDetailFragment : BaseFragment<FragmentRecommendTimeDetailBinding>(R.layout.fragment_recommend_time_detail) {
 
     override fun initStartView() {
-        (activity as MainActivity).setToolbar(true, "1차 회의")
+        (activity as MainActivity).setToolbar(true, "")
         addToToolbar()
 
     }
@@ -35,30 +36,39 @@ class RecommendTimeDetailFragment : BaseFragment<FragmentRecommendTimeDetailBind
 
     }
 
-
+    // 설정 버튼(이미지 버튼)을 포함한 툴바로 생성
     private fun addToToolbar() {
         // 툴바 찾기
         val toolbar: Toolbar = requireActivity().findViewById(R.id.toolbar)
         val layout: LinearLayout = toolbar.findViewById(R.id.layout_toolbar_btn)
 
-        val customButton = Button(requireContext())
+        // 이미지 버튼으로 생성
+        val imageButton = ImageButton(requireContext())
 
-        // 버튼 생성
-        customButton.apply{
-            text = "확인"
-            textSize = 17f
-            setTextColor(ContextCompat.getColor(context, R.color.mainBlack))
+        // 이미지 버튼 설정
+        imageButton.apply {
+            setImageResource(R.drawable.baseline_more_horiz_24)
             setBackgroundColor(ContextCompat.getColor(context, R.color.transparent))
+
+            // ImageButton에 LayoutParams 설정
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, // 너비
+                LinearLayout.LayoutParams.WRAP_CONTENT  // 높이
+            ).apply {
+                marginEnd = resources.getDimensionPixelSize(R.dimen.margin_btn_right) // 오른쪽 마진 설정
+            }
+            layoutParams = params
         }
 
-        // 툴바에 버튼 추가 -> 이미지 버튼으로 변경 필요
+        // 툴바의 기존 뷰들을 제거하고 이미지 버튼 추가
         layout.removeAllViews()
-        layout.addView(customButton)
+        layout.addView(imageButton)
 
-        // 버튼에 클릭 이벤트 추가
-        customButton.setOnClickListener {
-            //
+        // 이미지 버튼에 클릭 이벤트 추가
+        imageButton.setOnClickListener {
+            // 클릭 시 실행될 코드
         }
+
     }
 
 }
