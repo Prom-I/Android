8dedac3f37ec62bc54b692c56641990fcae312f1
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-02-18 11:59:49 +0900

    ADD : RecommendTimeDetail - RecommendTimeDetailRecyclerViewAdapter item

--- app/src/main/java/com/promi/view/promise/adapter/RecommendTimeDetailRecyclerViewAdapter.kt
+++ app/src/main/java/com/promi/view/promise/adapter/RecommendTimeDetailRecyclerViewAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.promi.R
-import com.promi.data.remote.model.RecommendDate
+import com.promi.data.remote.model.RecommendTimeDetail
 
 // 추천 약속 시간 상세 페이지
 // 가능한 친구
@@ -15,11 +15,7 @@ class RecommendTimeDetailRecyclerViewAdapter()
     : RecyclerView.Adapter<RecommendTimeDetailRecyclerViewAdapter.ViewHolder>(){
 
     // 해당 리사이클러뷰 아이템 리스트
-    private var recommendTimeDetailList : List<RecommendDate> = emptyList()
-    // 가능한 친구
-    //private var recommendDateList : List<RecommendDate> = emptyList()
-    // 불가능한 친구
-
+    private var recommendTimeDetailList : List<RecommendTimeDetail> = emptyList()
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvRecommend : TextView = itemView.findViewById(R.id.tv_recommend_time) // 9:00~10:00 약속 추천시간
@@ -40,19 +36,30 @@ class RecommendTimeDetailRecyclerViewAdapter()
 //        holder.tvRecommend.text = "추천 ${position+1}" // 추천 1
 //        holder.tvRecommendTime.text = recommendDate.recommendDate
 //        holder.btnGreat.text = recommendDate.likeCount.toString()
-//
-//        holder.itemView.setOnClickListener {
-//            listener.onRecommendTimeItemClicked(position) // 약속 페이지로 넘어가는 로직 작성 필요
-//        }
+        // 추천 시간
+        // 가능한 친구, 불가능한 친구
+
+        // 해당 아이템의 가능한 친구, 불가능한 친구 리스트로 가져와서 각 리사이클러뷰에 바인딩
+        // 가능한 친구, 불가능한 친구 리사이클러뷰는 그리디 레이아웃으로 그려야함
+
     }
+
 
     override fun getItemCount(): Int {
         return recommendTimeDetailList.size
     }
 
-    fun updateData(recommendDate: List<RecommendDate>) {
-        this.recommendTimeDetailList = recommendDate
+    fun updateData(recommendTimeDetailList: List<RecommendTimeDetail>) {
+        this.recommendTimeDetailList = recommendTimeDetailList
     }
+
+//    private fun setupRecyclerView() {
+//        participantsRecyclerViewAdapter = PromiseParticipantsRecyclerViewAdapter(participants)
+//        binding.recyclerviewPromiseMember.apply {
+//            adapter = participantsRecyclerViewAdapter
+//            layoutManager = GridLayoutManager(context, 5) // 한 줄에 5개의 아이템이 보이도록 설정
+//        }
+//    }
 
 
 }
