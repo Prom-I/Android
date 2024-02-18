c064b1550f1f77ea1ba3f591ddfe128f5617804e
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-02-18 12:15:18 +0900

    FEAT : RecommendTimeDetailRecyclerViewAdapter

--- app/src/main/java/com/promi/view/promise/adapter/RecommendTimeDetailRecyclerViewAdapter.kt
+++ app/src/main/java/com/promi/view/promise/adapter/RecommendTimeDetailRecyclerViewAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.promi.R
 import com.promi.data.remote.model.RecommendTimeDetail
@@ -18,11 +19,11 @@ class RecommendTimeDetailRecyclerViewAdapter()
     private var recommendTimeDetailList : List<RecommendTimeDetail> = emptyList()
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvRecommend : TextView = itemView.findViewById(R.id.tv_recommend_time) // 9:00~10:00 약속 추천시간
+        val tvRecommendTime : TextView = itemView.findViewById(R.id.tv_recommend_time) // 9:00~10:00 약속 추천시간
         val tvAbleFriendCount : TextView = itemView.findViewById(R.id.tv_able_friend_count) // 가능한 사람 수
-        val recyclerViewAbleFriend : RecyclerView = itemView.findViewById(R.id.recyclerview_able_friend) // 약속에 참여 가능한 사람
+        var recyclerViewAbleFriend : RecyclerView = itemView.findViewById(R.id.recyclerview_able_friend) // 약속에 참여 가능한 사람
         val tvDisableFriendCount : TextView = itemView.findViewById(R.id.tv_disable_friend_count) // 불가능한 사람 수
-        val recyclerViewDisableFriend : RecyclerView = itemView.findViewById(R.id.recyclerview_disable_friend) // 약속 참여 불가능한 사람
+        var recyclerViewDisableFriend : RecyclerView = itemView.findViewById(R.id.recyclerview_disable_friend) // 약속 참여 불가능한 사람
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -32,16 +33,23 @@ class RecommendTimeDetailRecyclerViewAdapter()
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val recommendDate = recommendDateList[position]
-//        holder.tvRecommend.text = "추천 ${position+1}" // 추천 1
-//        holder.tvRecommendTime.text = recommendDate.recommendDate
-//        holder.btnGreat.text = recommendDate.likeCount.toString()
-        // 추천 시간
-        // 가능한 친구, 불가능한 친구
+        val recommendTimeDetail = recommendTimeDetailList[position]
+        val ableFriendList = recommendTimeDetail.ableFriendList
+        val disableFriendList = recommendTimeDetail.disableFriendList
 
-        // 해당 아이템의 가능한 친구, 불가능한 친구 리스트로 가져와서 각 리사이클러뷰에 바인딩
-        // 가능한 친구, 불가능한 친구 리사이클러뷰는 그리디 레이아웃으로 그려야함
-
+        holder.tvRecommendTime.text = recommendTimeDetail.recommendTime // 추천 시간
+        // 가능한 친구
+        holder.tvAbleFriendCount.text = ableFriendList.size.toString()
+        holder.recyclerViewAbleFriend.apply {
+            adapter = PromiseParticipantsRecyclerViewAdapter(ableFriendList)
+            layoutManager = GridLayoutManager(context, 5) // 한 줄에 5개의 아이템이 보이도록 설정
+        }
+        // 불가능한 친구
+        holder.tvDisableFriendCount.text = disableFriendList.size.toString()
+        holder.recyclerViewDisableFriend.apply {
+            adapter = PromiseParticipantsRecyclerViewAdapter(disableFriendList)
+            layoutManager = GridLayoutManager(context, 5) // 한 줄에 5개의 아이템이 보이도록 설정
+        }
     }
 
 
@@ -52,14 +60,6 @@ class RecommendTimeDetailRecyclerViewAdapter()
     fun updateData(recommendTimeDetailList: List<RecommendTimeDetail>) {
         this.recommendTimeDetailList = recommendTimeDetailList
     }
-
-//    private fun setupRecyclerView() {
-//        participantsRecyclerViewAdapter = PromiseParticipantsRecyclerViewAdapter(participants)
-//        binding.recyclerviewPromiseMember.apply {
-//            adapter = participantsRecyclerViewAdapter
-//            layoutManager = GridLayoutManager(context, 5) // 한 줄에 5개의 아이템이 보이도록 설정
-//        }
-//    }
 
 
 }
